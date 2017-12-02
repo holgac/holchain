64246a1c9a95466c876c3e97066f25c8b6aa16e9
Huseyin Olgac <huseyinolgac@gmail.com>
2017-12-02 07:17:47 -0500

    logger module

--- node/app.js
+++ node/app.js
@@ -9,6 +9,7 @@ var stylus = require('stylus');
 var index = require('./routes/index');
 var redirect = require('./routes/redirect');
 var localdb = require('./routes/localdb');
+var log = require('./routes/log');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/redirect', redirect);
 app.use('/localdb', localdb);
+app.use('/log', log);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
