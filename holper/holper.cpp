a437d0f4f1a1ea1b589314a9444d47b44bff1820
huseyin <huseyinolgac@gmail.com>
2020-03-21 06:27:46 +0300

    holper pt2: parser thread

--- holper/holper.cpp
+++ holper/holper.cpp
@@ -10,8 +10,13 @@
 #include <cstdarg>
 #include <cstdlib>
 #include <vector>
+#include <list>
 #include <memory>
 #include <ctime>
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+#define SUCC_OR_RET(lg, fn) if(0 != fn) { (lg).log(Logger::ERROR, "%s failed", #fn); return 1; }
 
 class LogTarget
 {
@@ -27,7 +32,6 @@ private:
   bool close_;
 public:
   FDLogTarget(int fd, bool close) : fd_(fd), close_(close) {}
-
   virtual ~FDLogTarget() {
     if (close_) {
         close(fd_);
@@ -62,6 +66,7 @@ public:
     MUSTFIX = 3,
     ERROR = 4,
     FATAL = 5,
+    INTERNAL = 6,
   };
 private:
   std::vector<std::unique_ptr<LogTarget>> targets_;
@@ -82,7 +87,6 @@ public:
   void addTarget(LogTarget* target) {
     targets_.push_back(std::unique_ptr<LogTarget>(target));
   }
-
   void log(Level level, const char* format, ...) {
     const size_t BUFLEN = 2048;
     char buf[BUFLEN];
@@ -91,15 +95,23 @@ public:
     struct tm tm;
     localtime_r(&t, &tm);
     size_t size = strftime(buf, BUFLEN, "%Y-%m-%d %H:%M:%S", &tm);
+    char threadname[32];
+    pthread_getname_np(pthread_self(), threadname, 32);
     size += snprintf(buf + size, BUFLEN - size,
-        " [%lld]: %s", (long long)getpid(), levelColors_.at(level));
+        " [%lld:%lld,%s]: %s", (long long)getpid(), (long long)gettid(),
+        threadname, levelColors_.at(level));
     va_start(ap, format);
     int vsnlen = vsnprintf(buf + size, BUFLEN - size, format, ap);
     va_end(ap);
     int deflen = strlen(Consts::TerminalColors::DEFAULT);
     size += vsnlen;
     if (vsnlen < 0 || size + deflen + 1 >= BUFLEN) {
-      _log("CAN'T LOG", 9);
+      if (level != INTERNAL) {
+        log(INTERNAL, "Can't log \"%s\"", format);
+      } else {
+        std::string msg("Can't even log the format string");
+        _log(msg.c_str(), msg.size());
+      }
       return;
     }
     memcpy(buf + size, Consts::TerminalColors::DEFAULT, deflen);
@@ -107,10 +119,216 @@ public:
     _log(buf, size + deflen + 1);
   }
 };
+
+struct Context {
+public:
+  Logger logger;
+  Context()
+  {
+  }
+};
+
+
+class LockMutex {
+private:
+  pthread_mutex_t* mutex_;
+public:
+  LockMutex(pthread_mutex_t* mutex) : mutex_(mutex) {
+    pthread_mutex_lock(mutex_);
+  }
+  ~LockMutex() {
+    pthread_mutex_unlock(mutex_);
+  }
+};
+
+class ThreadBase
+{
+public:
+  virtual void start() = 0;
+};
+
+template <typename T>
+class Thread : public ThreadBase
+{
+private:
+  std::list<std::unique_ptr<T>> messages_;
+  pthread_t thread_;
+  pthread_mutex_t messagesMutex_;
+  sem_t messagesSemaphore_;
+  bool running_;
+  std::string name_;
+protected:
+  Context* context_;
+public:
+  Thread(std::string name, Context* context) : name_(name), context_(context) {}
+  void sendMessage(T* t) {
+    {
+      LockMutex lock(&messagesMutex_);
+      messages_.push_back(std::unique_ptr<T>(t));
+    }
+    sem_post(&messagesSemaphore_);
+  }
+  virtual void handleMessage(std::unique_ptr<T> msg) = 0;
+  void stop() {
+    running_ = false;
+  }
+  virtual void start() {
+    // TODO: error handling
+    thread_ = pthread_self();
+    pthread_setname_np(thread_, name_.c_str());
+    context_->logger.log(Logger::INFO, "Thread %s starting", name_.c_str());
+    pthread_mutex_init(&messagesMutex_, NULL);
+    sem_init(&messagesSemaphore_, 0, 0);
+    running_ = true;
+    while(running_) {
+      sem_wait(&messagesSemaphore_);
+      std::unique_ptr<T> msg;
+      {
+        LockMutex lock(&messagesMutex_);
+        msg = std::move(messages_.front());
+        messages_.pop_front();
+      }
+      handleMessage(std::move(msg));
+    }
+  }
+};
+
+void* start_thread(void* thread_ptr) {
+  printf("start_thread!\n");
+  ThreadBase* thread = (ThreadBase*)thread_ptr;
+  thread->start();
+  return NULL;
+}
+
+struct ParserArgs {
+  int fd;
+  ParserArgs(int fileDescriptor) : fd(fileDescriptor) {}
+};
+
+class Parser : public Thread<ParserArgs>
+{
+public:
+  Parser(std::string name, Context* context) : Thread(name, context) {}
+  void handleMessage(std::unique_ptr<ParserArgs> msg) {
+    int fd = msg->fd;
+    char buf[1024];
+    int res = recv(fd, buf, 1024, 0);
+    context_->logger.log(Logger::INFO, "fd %d received %s (%d)", fd, buf, res);
+    send(fd, "HELLO\n", 6, 0);
+    close(fd);
+  }
+};
+
+class Server
+{
+private:
+  std::string path_;
+  int socket_;
+  // TODO: shared_ptr?
+  Context context_;
+  bool running_ = true;
+  Parser* parser_;
+  int initLogger() {
+    if (isatty(STDOUT_FILENO)) {
+      context_.logger.addTarget(new FDLogTarget(STDOUT_FILENO, false));
+    }
+    char logdir[1024];
+    sprintf(logdir, "/run/user/%d/holperd.log", getuid());
+    int logfd = open(logdir, O_APPEND | O_CREAT | O_DIRECT | O_DSYNC, S_IRUSR | S_IWUSR);
+    if (!logfd) {
+      return 1;
+    }
+    context_.logger.addTarget(new FDLogTarget(logfd, true));
+    return 0;
+  }
+  int validateSocketPath() {
+    struct stat statbuf;
+    if (0 != stat(path_.c_str(), &statbuf)) {
+      context_.logger.log(Logger::INFO,
+          "%s does not exist, will create", path_.c_str());
+    } else {
+      if(0 != unlink(path_.c_str())) {
+        char errbuf[1024];
+        strerror_r(errno, errbuf, 1024);
+        context_.logger.log(Logger::FATAL,
+            "%s already exists, unlink failed with %s",
+            path_.c_str(), errbuf);
+        return 1;
+      }
+      context_.logger.log(Logger::INFO,
+          "%s already exists, unlinked", path_.c_str());
+    }
+    return 0;
+  }
+  int createSocket() {
+    socket_ = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (socket < 0) {
+      char errbuf[1024];
+      strerror_r(errno, errbuf, 1024);
+      context_.logger.log(Logger::FATAL,
+          "Socket creation failed: %s",
+          path_.c_str(), errbuf);
+      return 1;
+    }
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path_.c_str(), sizeof(addr.sun_path)-1);
+    if (0 != bind(socket_, (struct sockaddr*)&addr, sizeof(addr))) {
+      char errbuf[1024];
+      strerror_r(errno, errbuf, 1024);
+      context_.logger.log(Logger::FATAL,
+          "Socket bind failed: %s",
+          errbuf);
+      return 1;
+    }
+    return 0;
+  }
+public:
+  Context* getContext() { return &context_; }
+  Server(std::string path) : path_(path) {
+  }
+  int init() {
+    SUCC_OR_RET(context_.logger, initLogger())
+    SUCC_OR_RET(context_.logger, validateSocketPath())
+    SUCC_OR_RET(context_.logger, createSocket())
+    context_.logger.log(Logger::INFO, "renaming thread");
+    SUCC_OR_RET(context_.logger, pthread_setname_np(pthread_self(), "MainServer"))
+    context_.logger.log(Logger::INFO, "renamed thread");
+    parser_ = new Parser("Parser", &context_);
+    pthread_t tmp;
+    context_.logger.log(Logger::INFO, "starting thread");
+    pthread_create(&tmp, NULL, start_thread, (void*)parser_);
+    context_.logger.log(Logger::INFO, "started thread");
+    context_.logger.log(Logger::INFO, "Initialisation complete");
+    return 0;
+  }
+  void start() {
+    if(0 != listen(socket_, 10)) {
+      context_.logger.log(Logger::FATAL, "Listen failed");
+    }
+    while (running_) {
+      int fd = accept(socket_, NULL, NULL);
+      if (fd < 0) {
+        char errbuf[1024];
+        strerror_r(errno, errbuf, 1024);
+        context_.logger.log(Logger::FATAL,
+            "Accept failed: %s", errbuf);
+        continue;
+      }
+      context_.logger.log(Logger::INFO, "Got a new connection");
+      parser_->sendMessage(new ParserArgs(fd));
+    }
+  }
+  void setRunning(bool running) {
+    running_ = running;
+  }
+};
+
 int run_server(int argc, char** argv) {
-  char username[512];
-  getlogin_r(username, 512);
-  std::string socket_path = "/run/user/" + std::string(username) + "/holperd.sock";
+  char socketpathraw[512];
+  sprintf(socketpathraw, "/run/user/%d/holperd.sock", getuid());
+  std::string socket_path(socketpathraw);
   boost::program_options::options_description desc("Options");
   desc.add_options()
     ("help", "help help")
@@ -123,11 +341,9 @@ int run_server(int argc, char** argv) {
       vm);
   boost::program_options::notify(vm);
   std::cout << "socket-path: " << vm["socket-path"].as<std::string>() << std::endl;
-  Logger logger;
-  logger.addTarget(new FDLogTarget(STDOUT_FILENO, false));
-  logger.log(Logger::INFO, "info hey %d", 5);
-  logger.log(Logger::WARN, "warn hey %d", 1);
-  logger.log(Logger::ERROR, "error hey %d", 2);
+  Server server(vm["socket-path"].as<std::string>());
+  SUCC_OR_RET(server.getContext()->logger, server.init())
+  server.start();
   return 0;
 }
 
